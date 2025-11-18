c482c4b69c88e7f800943512a5e2894ede4a8ff3
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2025-11-18 01:57:59 +0000

    Initial Project Setup

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// when you visit this page, runs a function
+// declared route that LISTENS for GET requests to the root URL ('/')
+// function returns a view called welcome, check resources/views/welcome.blade.php
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Ep2 (time 4:40) - added a new route for /about page
+Route::get('/about', function () {
+    return view('about');
+});
