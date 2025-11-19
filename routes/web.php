e082df6df17afe14e6dee2b8bd7304bd2cbae203
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2025-11-19 17:38:06 +0000

    seperated code for the jobs array so the routes file becomes easier to read, understand and maintain. (web.php)

--- routes/web.php
+++ routes/web.php
@@ -3,47 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
-Route::get('/', function () {
-    return view('home', [
-    'greeting' => 'Hello, welcome to the home page!', // access to a variable $greeting
-    'name' => 'DavBot',
-    ]);
-})->name('home');
-
-// amended the about page to jobs page, reflected in the view too (resources/views/jobs.blade.php)
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'greeting' => 'Hello, welcome to the jobs page!',
-        'name' => 'DavBot',
-
-        'jobs' =>
-        [
+class Job {
+    public static function all() : array {
+        return [
             [
-                'id' => 1, // added unique identifier for each job, helps create hooks later on
-                'title' => 'Web Developer',
-                'salary' => '£50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Designer',
-                'salary' => '£60,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Content Creator',
-                'salary' => '£70,000',
-            ] // no need to end on a comma.
-        ]
-    ]);
-})->name('jobs');
-
-// need to create a route for the above > Listen for get requests to jobs/id
-// Laravel detects the thing wrapped in braces as a wild card
-Route::get('/jobs/{id}', function ($id) {
-    $jobs =
-        [
-            [
-                'id' => 1, // added unique identifier for each job, helps create hooks later on
+                'id' => 1,
                 'title' => 'Web Developer',
                 'salary' => '£50,000',
             ],
@@ -58,11 +22,30 @@ Route::get('/jobs/{id}', function ($id) {
                 'salary' => '£70,000',
             ]
         ];
+    }
+}
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/', function () {
+    return view('home', [
+    'greeting' => 'Hello, welcome to the home page!', // access to a variable $greeting
+    'name' => 'DavBot',
+    ]);
+})->name('home');
 
-    // dd($job);
-    
+// amended the about page to jobs page, reflected in the view too (resources/views/jobs.blade.php)
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'greeting' => 'Hello, welcome to the jobs page!',
+        'name' => 'DavBot',
+
+        'jobs' => Job::all()
+    ]);
+})->name('jobs');
+
+// need to create a route for the above > Listen for get requests to jobs/id
+// Laravel detects the thing wrapped in braces as a wild card
+Route::get('/jobs/{id}', function ($id) {
+    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
     return View('job', ['job' => $job]);
 })->name('job.show');
 
