406157cb16c6fa1fe1e688bab579908919af6142
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2025-11-26 17:57:39 +0000

    removed redundant comments, going to shorten this down to avoid repeated info

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 use App\Http\Controllers\JobController;
 
 Route::get('/', function () {
-    return view('home', [
-        'greeting' => 'Hello, welcome to the home page!',
-        'name' => 'DavBot'
-    ]);
+    return view('home');
 })->name('home');
 
 Route::get('/contact', function () {
@@ -18,25 +14,12 @@ Route::get('/contact', function () {
     ]);
 })->name('contact');
 
-// index
 Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
-
-// create
 Route::get('/jobs/create', [JobController::class, 'create'])->name('job');
-
-//show
 Route::get('/jobs/{job}', [JobController::class, 'show'])->name('job');
-
-// store
 Route::post('/jobs', [JobController::class, 'store'])->name('job');
-
-// edit
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->name('job');
-
-// update (uses same uri as show, but different method)
 Route::patch('/jobs/{job}', [JobController::class, 'update'])->name('job');
-
-// destroy
 Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->name('job');
 
 
